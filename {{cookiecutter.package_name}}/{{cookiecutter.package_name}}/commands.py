c59401aefe6ebc26bb3063d16498edf8ff278ac3
juanda <juanda@yuido.com>
2017-02-14 18:20:26 +0100

    command export_api to help with test in postman

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/commands.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/commands.py
@@ -11,3 +11,14 @@ def example_command():
     """
     click.echo('This is an example command')
 
+app.config['SERVER_NAME'] = 'localhost:5000'
+
+@app.cli.command()
+def export_api():
+    """To help you testing, you can export your API as a Postman collection."""
+    with app.app_context():
+        urlvars = False  # Build query strings in URLs
+        swagger = True  # Export Swagger specifications
+        data = api.as_postman(urlvars=urlvars, swagger=swagger)
+        f = open('postman_import.json', 'w')
+        f.write(json.dumps(data))
