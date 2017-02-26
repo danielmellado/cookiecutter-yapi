eb79b120a33b152e07dfe04af9866fd030d9c229
Juan David Rodríguez <juandavid.rodriguez@ite.educacion.es>
2017-02-26 06:47:54 +0100

    Update crud_resource.py

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/resources/crud_resource.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/resources/crud_resource.py
@@ -18,10 +18,9 @@ from pprint import pprint
 from flask import request, jsonify, make_response, abort
 from flask_restplus import Resource
 
-from .. import api
+from .. import  api
 
-
-def get_crud_resource(db, Entity, get_schema, put_schema, resource_fields):
+def get_crud_resource(db, Entity, get_schema, put_schema, resource_fields=None):
 
     @api.doc(params={'id': 'The table identifier'})
     class CrudResource(Resource):
@@ -31,10 +30,10 @@ def get_crud_resource(db, Entity, get_schema, put_schema, resource_fields):
             404: 'Not Found'
         })
         def get(self, id):
-            object = Entity.query.get(id)
-            if object is None:
+            obj = Entity.query.get(id)
+            if obj is None:
                 return abort(404)
-            result = get_schema.dump(object)
+            result = get_schema.dump(obj)
             return jsonify(result.data)
 
         @api.doc(responses={
@@ -42,11 +41,13 @@ def get_crud_resource(db, Entity, get_schema, put_schema, resource_fields):
             404: 'Not Found'
         })
         def delete(self, id):
-            object = Entity.query.get(id)
-            if object is None:
+            obj = Entity.query.get(id)
+            self.pre_delete(obj)
+            if obj is None:
                 return abort(404)
-            db.session.delete(object)
+            db.session.delete(obj)
             db.session.commit()
+            # No hacemos post delete pues después de borrar no se debería hacer nada
             return {"id": id}
 
         @api.doc(
@@ -58,45 +59,57 @@ def get_crud_resource(db, Entity, get_schema, put_schema, resource_fields):
             }
         )
         def put(self, id):
-            object = Entity.query.get(id)
-            if object is None:
+            obj = Entity.query.get(id)
+            self.pre_update(obj)
+            if obj is None:
                 return abort(404)
             json_data = request.get_json(force=True)
-            put_schema.context['object'] = object
+            put_schema.context['obj'] = obj
             data, errors = put_schema.load(json_data)
 
             if any(errors):
                 return make_response(jsonify(errors), 422)
 
-            object.set(data)
-            db.session.add(object)
+            obj.set(data)
+            db.session.add(obj)
             db.session.commit()
+            self.post_update(obj)
 
-            return put_schema.dump(object)
+            return put_schema.dump(obj)
+
+        def pre_update(self, obj):
+            pass
+
+        def post_update(self, obj):
+            pass
+
+        def pre_delete(self, obj):
+            pass
+
 
     return CrudResource
 
 
-def get_crud_list(db, Entity, get_collection_schema, post_schema, resource_fields):
+def get_crud_list(db, Entity, get_collection_schema, post_schema, resource_fields=None):
 
     class CrudList(Resource):
 
-        def query(self, args):
+        def _query(self, args):
             if 'filters' in args:
                 filters = json.loads(args['filters'])
-                objects = Entity.query.filter_by(**filters)
+                objs = Entity.query.filter_by(**filters)
             else:
-                objects = Entity.query.all()
+                objs = Entity.query.all()
 
-            return objects
+            return objs
 
         @api.doc(responses={
             200: 'Success'
         })
         def get(self):
             # pprint(json.loads(request.args['filters']))
-            objects = self.query(request.args)
-            result = get_collection_schema.dump(objects)
+            objs = self._query(request.args)
+            result = get_collection_schema.dump(objs)
             return jsonify(result.data)
 
         @api.doc(
@@ -107,15 +120,23 @@ def get_crud_list(db, Entity, get_collection_schema, post_schema, resource_field
             }
         )
         def post(self):
+            self.pre_create()
             json_data = request.get_json(force=True)
             data, errors = post_schema.load(json_data)
 
             if any(errors):
                 return make_response(jsonify(errors), 422)
 
-            object = Entity(data)
-            db.session.add(object)
+            obj = Entity(data)
+            db.session.add(obj)
             db.session.commit()
-            return make_response(jsonify(post_schema.dump(obj)), 201)
+            self.post_create(obj)
+            return make_response(jsonify(post_schema.dump(obj).data), 201)
+
+        def pre_create(self):
+            pass
+
+        def post_create(self, obj):
+            pass
 
     return CrudList
